48f6ca888d3fb627c3decddfc47bbdad7796628f
milipara <pjun1106@gmail.com>
2024-04-23 23:47:15 +0900

    0423

---
+++ 문자열/11654_아스키 코드/11654_아스키 코드.py
@@ -0,0 +1,16 @@
+import sys
+
+sys.stdin = open("11654_아스키 코드.txt")
+
+T = input()
+print(ord(T))
+T = input()
+print(ord(T))
+T = input()
+print(ord(T))
+T = input()
+print(ord(T))
+T = input()
+print(ord(T))
+T = input()
+print(ord(T))
