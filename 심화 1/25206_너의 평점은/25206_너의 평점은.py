1613210f4bf2fac64bd3bf28189509ac35a36983
milipara <pjun1106@gmail.com>
2024-05-19 23:40:18 +0900

    0519

--- 심화 1/25206_너의 평점은/25206_너의 평점은.py
+++ 심화 1/25206_너의 평점은/25206_너의 평점은.py
@@ -7,9 +7,21 @@ total=0
 sum=0
 
 for i in range(20):
-    I,S,R = input().split()
-    if R == "P":
-        continue
-    total += float(S)
-    sum += 1
+    I,S,R = input().split() #각 과목 학점 등급으로 나누기
+    if R == "P": # 등급이 P일 경우
+        continue # 넘기기
+    total += float(S) * rank[R] # 학점 * 랭크점수 더하기
+    sum += float(S) # 학점 더하기
+print(total/sum)
+
+rank = {'A+':4.5, 'A0':4.0,'B+':3.5,'B0':3.0,'C+':2.5,'C0':2.0,'D+':1.5,'D0':1.0,'F':0}
+total=0
+sum=0
+
+for i in range(20):
+    I,S,R = input().split() #각 과목 학점 등급으로 나누기
+    if R == "P": # 등급이 P일 경우
+        continue # 넘기기
+    total += float(S) * rank[R] # 학점 * 랭크점수 더하기
+    sum += float(S) # 학점 더하기
 print(total/sum)
