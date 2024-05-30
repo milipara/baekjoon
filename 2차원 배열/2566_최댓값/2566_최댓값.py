84cca2092fb6a2b5f87b2bab760980bf7fff69a9
milipara <pjun1106@gmail.com>
2024-05-30 23:52:48 +0900

    0530

--- 2차원 배열/2566_최댓값/2566_최댓값.py
+++ 2차원 배열/2566_최댓값/2566_최댓값.py
@@ -2,21 +2,18 @@ import sys
 
 sys.stdin = open("2566_최댓값.txt")
 
-A = []
+A = [] #빈 배열 만들기
 for i in range(9):
-    A.append(list(map(int,input().split())))
-row = 0
-col = 0
+    A.append(list(map(int,input().split()))) #
+row = 0 #열
+col = 0 #행
+max_val=0 #최대치 변수
 
-for i in range(N):
-    i = list(map(int,input().split())) # A배열에 append로 쌓기
-    A.append(i)
-
-for j in range(N):
-    j = list(map(int,input().split())) # B배열에 append로 쌓기
-    B.append(j)
-
-for k in range(N):
-    for l in range(M):
-        print(A[k][l] + B[k][l], end= ' ')
-    print()
+for X in range(9): # X축 반복
+    for Y in range(9): # Y축 반복
+        if A[X][Y] >= max_val: # 최대치랑 X,Y축과 비교
+            max_val = A[X][Y] # 그렇다면 X,Y축 숫자로 변경 그리고 X,Y축 입력
+            row = X+1
+            col = Y+1
+print(max_val)
+print(row,col)
