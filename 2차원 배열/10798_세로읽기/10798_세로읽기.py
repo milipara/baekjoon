362c22eca1731a5980d450929c4232fedca46800
milipara <pjun1106@gmail.com>
2024-06-09 23:46:56 +0900

    0609

--- 2차원 배열/10798_세로읽기/10798_세로읽기.py
+++ 2차원 배열/10798_세로읽기/10798_세로읽기.py
@@ -4,11 +4,13 @@ sys.stdin = open("10798_세로읽기.txt")
 
 words = []
 list = ""
+num=[]
 for i in range(5):
     word=input()
     words.append(word)
+    num.append(len(word))
 
-for i in range(5):
+for i in range(num):
     for j in range(5):
-        list += words[i][j]
+        list += words[j][i]
 print(list)
